c05bccee72a02e6861f273e062db73f4afa003b0
Siya2005 <141432022+Siya2005@users.noreply.github.com>
2025-08-27 21:09:07 +0530

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -1,41 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int mergeAndCount(int arr[], int temp[], int left, int mid, int right) {
-    int i = left, j = mid, k = left, inv_count = 0;
-    while(i < mid && j <= right) {
-        if(arr[i] <= arr[j]) {
-            temp[k++] = arr[i++];
-        } else {
-            temp[k++] = arr[j++];
-            inv_count += mid - i;
-        }
-    }
-    while(i < mid) temp[k++] = arr[i++];
-    while(j <= right) temp[k++] = arr[j++];
-    for(i = left; i <= right; i++) arr[i] = temp[i];
-    return inv_count;
-}
-
-int mergeSortAndCount(int arr[], int temp[], int left, int right) {
-    int inv_count = 0;
-    if(left < right) {
-        int mid = (left + right)/2;
-        inv_count += mergeSortAndCount(arr, temp, left, mid);
-        inv_count += mergeSortAndCount(arr, temp, mid+1, right);
-        inv_count += mergeAndCount(arr, temp, left, mid+1, right);
-    }
-    return inv_count;
-}
-
 int main() {
     int n;
     cout << "Enter number of elements: ";
     cin >> n;
-    int arr[n], temp[n];
+
+    int arr[n];
     cout << "Enter elements:\n";
-    for(int i=0; i<n; i++) cin >> arr[i];
-    int result = mergeSortAndCount(arr, temp, 0, n-1);
-    cout << "Number of inversions: " << result << endl;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int inversionCount = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j])
+                inversionCount++;
+        }
+    }
+
+    cout << "Total number of inversions: " << inversionCount << endl;
     return 0;
 }
